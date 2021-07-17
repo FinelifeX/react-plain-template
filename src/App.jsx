982605d711f703d37537b1ed2e8fbb54ddc991bf
Bulat Murtazin <bulatf.murtazin@gmail.com>
2021-07-17 15:00:30 +0300

    = add React component template

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React fresh setup!</h1>
+      <p>
+        This setup doesn't use Create React App!
+      </p>
+    </>
+  )
+}
+
+export default App;
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-
-const app = document.getElementById('app');
-
-render(
-  <div>Hello!</div>,
-  app,
-);
